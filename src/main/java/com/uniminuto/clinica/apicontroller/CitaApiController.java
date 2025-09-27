6455a29a164f8f99df2706590a34db658d39371a
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-09-27 11:52:51 -0500

    Create CitaApiController.java

---
+++ src/main/java/com/uniminuto/clinica/apicontroller/CitaApiController.java
@@ -0,0 +1,27 @@
+package com.uniminuto.clinica.apicontroller;
+
+import com.uniminuto.clinica.api.CitaApi;
+import com.uniminuto.clinica.entity.Cita;
+import com.uniminuto.clinica.model.CitaRq;
+import com.uniminuto.clinica.service.CitaService;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CitaApiController implements CitaApi {
+    @Autowired
+    CitaService citaService;
+    @Override
+    public ResponseEntity<String> guardarCita(CitaRq cita) throws BadRequestException {
+        return ResponseEntity.ok(this.citaService.guardarCita(cita));
+    }
+
+    @Override
+    public ResponseEntity<List<Cita>> listarRecientes() {
+        return ResponseEntity.ok(this.citaService.listarReciente());
+    }
+}
