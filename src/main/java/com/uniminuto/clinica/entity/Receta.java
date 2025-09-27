9ae1f9925fab0b3fe7ed0b23128b34b0ea89f2e5
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-09-27 11:56:11 -0500

    Create Receta.java

---
+++ src/main/java/com/uniminuto/clinica/entity/Receta.java
@@ -0,0 +1,32 @@
+package com.uniminuto.clinica.entity;
+
+import lombok.Data;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "receta")
+public class Receta implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "dosis")
+    private String dosis;
+    @Column(name = "fecha_registro")
+    private LocalDateTime fechaRegistro;
+    @Column(name = "indicaciones")
+    @Nullable
+    private String indicaciones;
+    @ManyToOne
+    @JoinColumn(name = "cita_id")
+    private Cita cita;
+    @ManyToOne
+    @JoinColumn(name = "medicamento_id")
+    private Medicamento medicamento;
+
+}
